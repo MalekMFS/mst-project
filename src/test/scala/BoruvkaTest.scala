00008bee2604d678a3858b778c69c000e46292bb
MalekMFS <malekabbasi@meam.ir>
2020-12-21 12:06:37 +0330

    added a tree test for Boruvka

--- src/test/scala/BoruvkaTest.scala
+++ src/test/scala/BoruvkaTest.scala
@@ -7,7 +7,7 @@ class BoruvkaTest extends AnyFunSuite {
   val edges: List[weightedEdge] = sampleEdges.map(r => weightedEdge( myEdge(r._1._1, r._1._2), r._2))
   val boruvka: List[weightedEdge] = Boruvka(edges)
   val correctResult = List(weightedEdge(myEdge(2,5),3), weightedEdge(myEdge(3,5),5), weightedEdge(myEdge(4,6),6), weightedEdge(myEdge(1,2),7), weightedEdge(myEdge(5,6),10), weightedEdge(myEdge(5,7),12))// :+ weightedEdge(myEdge(4,5),15)
-  test("Boruvka must return all the edges in correctResult for sampleEdges"){
+  test("Boruvka must return correct result for sampleEdges"){
     assert(boruvka.length == 6)
 
     correctResult.foreach { e =>
@@ -18,7 +18,7 @@ class BoruvkaTest extends AnyFunSuite {
   val sampleEdges2: List[weightedEdge] = List(((6,1),6), ((6,7),13), ((1,7),9), ((1,3),4), ((7,2),5), ((5,7), 11), ((2,5),8), ((3,7),7), ((4,7),2), ((3,4),6), ((3,5),10)).map(r => weightedEdge( myEdge(r._1._1, r._1._2), r._2))
   val boruvka2: List[weightedEdge] = Boruvka(sampleEdges2)
   val correctResult2: List[weightedEdge] = List(((6,1),6), ((1,3),4), ((7,2),5), ((2,5),8), ((4,7),2), ((3,4),6)).map(r => weightedEdge( myEdge(r._1._1, r._1._2), r._2))
-  test("Boruvka sampleEdges2"){
+  test("Boruvka must return correct result for sampleEdges2"){
     assert(boruvka2.length == 6)
     assert(boruvka2.map(_.weight).sum == 31)
 
@@ -31,12 +31,24 @@ class BoruvkaTest extends AnyFunSuite {
   val sampleEdges3: List[weightedEdge] = List(((1,2),13), ((1,3),6), ((3,2),7), ((2,4),1), ((3,5),8), ((3,4),14), ((4,5),9), ((4,6),3), ((5,6),2), ((3,8),20), ((5,10),18), ((7,8),15), ((7,9),5), ((8,10),17), ((9,11),11), ((11,10),16), ((10,12),4), ((11,12),12), ((10,7),19), ((11,7),10)).map(r => weightedEdge( myEdge(r._1._1, r._1._2), r._2))
   val boruvka3: List[weightedEdge] = Boruvka(sampleEdges3)
   val correctResult3: List[weightedEdge] = List(((1,3),6), ((2,4),1), ((3,2),7), ((4,6),3), ((5,6),2), ((7,8),15), ((7,9),5), ((10,12),4), ((11,12),12), ((11,7),10), ((5,10),18)).map(r => weightedEdge( myEdge(r._1._1, r._1._2), r._2))
-  test("Boruvka sampleEdges3"){
+  test("Boruvka must return correct result for sampleEdges3 the 'wikipedia sample graph'"){
     assert(boruvka3.length == 11)
     assert(boruvka3.map(_.weight).sum == correctResult3.map(_.weight).sum)
 
     correctResult3.foreach { e =>
       assert(boruvka3.contains(e))
+    }
+  }
+
+  val sampleEdges4: List[weightedEdge] = List(((1,3),6), ((2,3),7), ((4,3),5)).map(r => weightedEdge( myEdge(r._1._1, r._1._2), r._2))
+  val boruvka4: List[weightedEdge] = Boruvka(sampleEdges4)
+  val correctResult4: List[weightedEdge] = List(((1,3),6), ((2,3),7), ((4,3),5)).map(r => weightedEdge( myEdge(r._1._1, r._1._2), r._2))
+  test("Boruvka must return correct result for sampleEdges4 the 'Tree'"){
+    assert(boruvka4.length == correctResult4.length)
+    assert(boruvka4.map(_.weight).sum == correctResult4.map(_.weight).sum)
+
+    correctResult4.foreach { e =>
+      assert(boruvka4.contains(e))
     }
   }
 }
