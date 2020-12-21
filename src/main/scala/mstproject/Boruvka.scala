b1be331bd6446a89c0bb6f51583abae19a2ba9d6
MalekMFS <malekabbasi@meam.ir>
2020-12-21 13:47:25 +0330

    Beta version of Boruvka, tested.

--- src/main/scala/mstproject/Boruvka.scala
+++ src/main/scala/mstproject/Boruvka.scala
@@ -5,18 +5,17 @@ import Model._
 import scala.annotation.tailrec
 import scala.collection.mutable.ListBuffer
 
+/** Returns MST for Connected graphs */
 object Boruvka {
 //  def demo[T: Monoid](ls: List[T]) =
 //    ls.foldLeft(Monoid[T].empty) (_ |+| _)
 
   def apply(E : List[weightedEdge]): List[weightedEdge] = {
-    //TODO use bloomFilter
-    //FIXME tikiDisjointSet's find method seems non-optimal. fix or replace it.
 
     /** Vertex -> List of Edges map, and list of vertices */
     val t1 = E.map(e => (e.edge.u, e))
     val t2 = E.map(e => (e.edge.v, e))
-    val vToE = (t1 ++ t2).groupBy(_._1).mapValues(list => list.map(_._2)) // a map from Vertex to Edges connected to vertex
+    val vToE = (t1 ++ t2).groupBy(_._1).mapValues(list => list.map(_._2)) // a map from Vertex to Edges connected to that vertex
     val vertices = ListBuffer[Int]()
 
     /** Initialize the DisjointSet. add a set for each vertex */
@@ -33,10 +32,13 @@ object Boruvka {
 
     /** While 'set' has more than one component */
     @tailrec
-    def BoruvkaLoop (vSet: tikiDisjointSet[Int], edgesMap: Map[Int, List[weightedEdge]], forest: List[weightedEdge]): List[weightedEdge] = {
-      //TODO check if edgesMap is nonempty for disconnected graph
-      if (vSet.components > 1) {
-        val sets = vSet.parents.groupBy(_._2).mapValues(_.keys) // a Map from Sets to corresponding vertices
+    def BoruvkaLoop (vSet: tikiDisjointSet[Int], edgesMap: Map[Int, List[weightedEdge]], forest: List[weightedEdge]): List[weightedEdge] =
+
+      if (vSet.components > 1) { // Connected Graph
+        val sets = vSet.parents
+          .map(x => (x._1, vSet.find(x._2))) // Parents could contain parents that are not self-parent.
+          .groupBy(_._2)
+          .mapValues(_.keys) // a Map from Sets to corresponding vertices
         /** foreach connected component select their minimum edge */
         val selectedEdges = sets.map { set =>
           val vertices = set._2
@@ -51,18 +53,18 @@ object Boruvka {
         /** Remove selected edges from the edgesMap for the next iterations */
         //TODO check if it's better to use `fold` instead of `foldLeft`
         val newEdgesMap = selectedEdges.foldLeft(edgesMap) { (m, e) =>
-          //TODO any special case that temp.length == 0 ?
-          val temp = m.-(e.edge.u, e.edge.v)
+          val tempMap = m.-(e.edge.u, e.edge.v)
           val eu = m(e.edge.u)  diff List(e)
           val ev = m(e.edge.v)  diff List(e)
+          /** re-add vertices with new edges */
           if (eu.nonEmpty && ev.nonEmpty)
-            temp + (e.edge.u -> eu, e.edge.v -> ev)
+            tempMap + (e.edge.u -> eu, e.edge.v -> ev)
           else if (eu.nonEmpty)
-            temp + (e.edge.u -> eu)
+            tempMap + (e.edge.u -> eu, e.edge.v -> List[weightedEdge]())
           else if (ev.nonEmpty)
-            temp + (e.edge.v -> ev)
+            tempMap + (e.edge.v -> ev, e.edge.u -> List[weightedEdge]())
           else
-            temp
+            tempMap + (e.edge.u -> List[weightedEdge](), e.edge.v -> List[weightedEdge]())
         }
 
         /** union sets in this iteration */
@@ -76,7 +78,6 @@ object Boruvka {
         BoruvkaLoop(newvSet, newEdgesMap, forest ++ selectedEdges.toList)
       }
       else forest
-    }
 
     BoruvkaLoop(s, vToE, List[weightedEdge]())
   }
