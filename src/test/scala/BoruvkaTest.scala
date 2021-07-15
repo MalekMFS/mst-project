51083c3ed898e6ac5a334b339915d6fa17ee13b7
MalekMFS <malekabbasi@meam.ir>
2021-07-15 11:22:23 +0430

    fixed Boruvka test error

--- src/test/scala/BoruvkaTest.scala
+++ src/test/scala/BoruvkaTest.scala
@@ -6,7 +6,7 @@ class BoruvkaTest extends AnyFunSuite {
   val sampleEdges = List(((1,4),14), ((3,5),5), ((4,6),6), ((1,2),7), ((2,5),3), ((2,3),8), ((5,6),10), ((2,4),11), ((5,7),12), ((6,7),13), ((4,5),15))
   val edges: List[weightedEdge] = sampleEdges.map(r => weightedEdge( myEdge(r._1._1, r._1._2), r._2, id = r._2))
   val boruvka: List[weightedEdge] = Boruvka(edges)
-  val correctResult = List(weightedEdge(myEdge(2,5),3, id = 1), weightedEdge(myEdge(3,5),5, id = 2), weightedEdge(myEdge(4,6),6, id = 3), weightedEdge(myEdge(1,2),7, id = 4), weightedEdge(myEdge(5,6),10, id = 5), weightedEdge(myEdge(5,7),12, id = 6))// :+ weightedEdge(myEdge(4,5),15)
+  val correctResult = List(weightedEdge(myEdge(2,5),3, id = 3), weightedEdge(myEdge(3,5),5, id = 5), weightedEdge(myEdge(4,6),6, id = 6), weightedEdge(myEdge(1,2),7, id = 7), weightedEdge(myEdge(5,6),10, id = 10), weightedEdge(myEdge(5,7),12, id = 12))// :+ weightedEdge(myEdge(4,5),15)
   test("Boruvka must return correct result for sampleEdges"){
     assert(boruvka.length == 6)
 
