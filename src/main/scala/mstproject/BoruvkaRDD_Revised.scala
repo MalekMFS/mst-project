761d63bd88dd2e544353706992309da485faa523
MalekMFS <malekabbasi@meam.ir>
2021-08-12 12:29:12 +0430

    improvements to BoruvkaRDD_Revised + Tries to repartition RDD

--- src/main/scala/mstproject/BoruvkaRDD_Revised.scala
+++ src/main/scala/mstproject/BoruvkaRDD_Revised.scala
@@ -2,6 +2,7 @@ package mstproject
 
 import mstproject.Model._
 import org.apache.log4j.Logger
+import org.apache.spark.Partitioner
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import redis.clients.jedis.Jedis
@@ -23,18 +24,27 @@ object BoruvkaRDD_Revised {
     log.warn(s"# components: $components")
     if(debug) { val mstCount = forest.count; log.warn(s"# input MST edges: $mstCount") }
 
+    println("partitions: " + setToE.getNumPartitions)
     if (components > 1) {
       /** foreach connected component select their minimum edge */
-      val VToECandidates = setToE.mapPartitions{ part =>
+      val VToECandidates: RDD[(Int, Iterable[weightedEdge])] = setToE
+//        .partitionBy(new Partitioner {
+//          override def numPartitions: Int = 100
+//
+//          override def getPartition(key: Any): Int = {
+//            key.asInstanceOf[Int] % numPartitions
+//          }
+//        })
+        .mapPartitions{ part =>
         part.map{ vEs =>
           val iteration: Int = RedisDisjointSet.iterationCount
           val vertex = vEs._1 // or set after the first round
-          val selectableEdges = vEs._2.filter(e => !e.removed && RedisDisjointSet.find(e.edge.u) != RedisDisjointSet.find(e.edge.v))
+          val selectableEdges = if(round > 1) vEs._2.filter(e => !e.removed && RedisDisjointSet.find(e.edge.u) != RedisDisjointSet.find(e.edge.v)) else vEs._2
           if (selectableEdges.nonEmpty){
-            val minEdge = selectableEdges.minBy(_.weight)
-            val updatedEdges  = vEs._2.groupBy(_.id).values.map(it => it.last) // remove duplicate edges
+            val minEdge = selectableEdges.minBy(_.weight) // of set (or vertex in first round)
+            val updatedEdges  = vEs._2.groupBy(_.id).values.map(it => it.last) // remove duplicate edges inside the set
               .map { e =>
-                if (!e.removed && e == minEdge) // and not in same set already checked
+                if (!e.removed && e.id == minEdge.id) // and not in same set already checked
                   if (iteration > 1)
                     weightedEdge(e.edge, e.weight, removed = false, selectedInStep = iteration, e.id)
                   else // First iteration: sets = vertices. deterministic choices.
@@ -50,33 +60,43 @@ object BoruvkaRDD_Revised {
         }
       } //.cache
       // TODO partition sets by vertices.
-      if(debug) { val numCandidates = VToECandidates.flatMap(_._2).filter(_.selectedInStep == round).count; log.warn(s"# Candidate edges to be removed (selected): $numCandidates") }
+      if(debug) { val numCandidates = VToECandidates.flatMap(_._2).filter(_.selectedInStep == round).count; log.warn(s"# Candidate and removed edges in this round: $numCandidates") }
 
-      val updatedSetToE: RDD[(Int, Iterable[weightedEdge])] = {
-        if (round > 1)
-          VToECandidates.groupBy(x => RedisDisjointSet.find(x._1)).map { sets =>
+      val updatedSetToE = {
+        if (round > 1) { // then some of the candidate edges are not the min edge of their set
+          //FIXME: should also merge sets after first round
+          VToECandidates.mapPartitions(part => part.map(c => (RedisDisjointSet.find(c._1), c._2))).groupBy(x => x._1)
+//            .partitionBy(new Partitioner {
+//              override def numPartitions: Int = 50
+//
+//              override def getPartition(key: Any): Int = {
+//                key.asInstanceOf[Int] % numPartitions
+//              }
+//            })
+            .map { sets => // merge sets to make a new
             val set = sets._1
             val edgesOfVertex = sets._2
             val selectableEdges = edgesOfVertex.flatten(_._2)
-              .groupBy(_.id).values.map(it => it.last) // remove duplicate edges
-              .filter(_.selectedInStep == round)
+              .filter(e => !e.removed && e.selectedInStep == round) // Candidates
+              .groupBy(_.id).values.map(it => it.last) // remove duplicate edges //FIXME redundant?
             if (selectableEdges.nonEmpty){
               val setMinEdge = selectableEdges.minBy(_.weight)
               val res = edgesOfVertex.flatMap{ thisSet =>
                 val edges = thisSet._2
                 edges.map { e =>
-                  if (!e.removed && e == setMinEdge)
+                  if (!e.removed && e.id == setMinEdge.id)
                     weightedEdge(e.edge, e.weight, removed = true, e.selectedInStep, e.id)
                   else e
                 }
               }.groupBy(_.id).values.map(it => it.last) // remove duplicate edges
-              (set.getOrElse(-1L).toInt, res)
+              (set.getOrElse(-1L).toInt, res) //FIXME do I need to handle -1 set?
             } else {
+              //TODO just filter the removed edges?
               val markedRemoved = edgesOfVertex.flatten(_._2).map(e => weightedEdge(e.edge, e.weight, removed = true, e.selectedInStep, e.id))
               (set.getOrElse(-1L).toInt, markedRemoved)
             }
           }
-        else VToECandidates
+        } else VToECandidates
       }.cache
       if(debug){ val numKeys = updatedSetToE.count; log.warn(s"# Keys (should be same as components): $numKeys") }
 
@@ -95,31 +115,31 @@ object BoruvkaRDD_Revised {
       BoruvkaLoop(updatedSetToE, forest ++ selectedEdges, debug) // next iteration after updates
     }
     else { // return the resulting forest
-      val mstWeight = forest.aggregate(0)((acc, e) => acc + e.weight, (acc, e) => acc + e)
+      val mstWeight = forest.aggregate(0L)((acc, e) => acc + e.weight, (acc, e) => acc + e)
       log.warn(s"Weight of MST: $mstWeight")
       log.warn("Finished. Now Collecting...")
       forest.collect().toList
     }
   }
 
-  def apply(path: String, delim: String, weighted: Boolean = false, debug: Boolean = false): List[weightedEdge] = {
+  def apply(dataset: graphDataset, debug: Boolean = false): List[weightedEdge] = {
     //TODO anywhere needed to cache data?
 
     lazy val spark: SparkSession = SparkConstructor()
     lazy val sc = spark.sparkContext
 //    val E: RDD[weightedEdge] = sc.parallelize(edges, 100) // number of partitions
 
-    val E: RDD[weightedEdge] = if (weighted)
+    val E: RDD[weightedEdge] = if (dataset.weighted)
       spark.read
-        .format("csv").option("delimiter",delim)
-        .load(path).toDF("u", "v", "w")
+        .format("csv").option("delimiter",dataset.delim)
+        .load(dataset.path).toDF("u", "v", "w")
         .rdd
         .zipWithIndex
         .map( r => weightedEdge( myEdge(r._1.getAs[String](0).toInt, r._1.getAs[String](1).toInt), r._1.getAs[String](2).toInt, id = r._2.toInt) )
       else
       spark.read
-        .format("csv").option("delimiter",delim)
-        .load(path).toDF("u", "v")
+        .format("csv").option("delimiter",dataset.delim)
+        .load(dataset.path).toDF("u", "v")
         .rdd
         .zipWithIndex // add weights
         .map( r => weightedEdge( myEdge(r._1.getAs[String](0).toInt, r._1.getAs[String](1).toInt), r._2.toInt, id = r._2.toInt) )
@@ -141,9 +161,16 @@ object BoruvkaRDD_Revised {
     /** Vertex to List of Edges map, and list of vertices */
     val t3 = E.map(e => vertexEdge(e.edge.u, e))
     val t4 = E.map(e => vertexEdge(e.edge.v, e))
-    val vToE = t3.union(t4)
+    val vToE: RDD[(Int, Iterable[weightedEdge])] = t3.union(t4)
       .groupBy(kv => kv.v)
       .mapValues(list => list.map(_.edge))
+//      .partitionBy(new Partitioner {
+//      override def numPartitions: Int = vCount.toInt
+//
+//      override def getPartition(key: Any): Int = {
+//        key.asInstanceOf[Int]
+//      }
+//    })
 
     val result = BoruvkaLoop(vToE, spark.sparkContext.emptyRDD[weightedEdge], debug)
     log.warn("Collected Result Successfully!")
