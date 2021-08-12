533e8010aae924e5639a4b4d02f45be0cf678061
MalekMFS <malekabbasi@meam.ir>
2021-08-12 12:26:50 +0430

    Added a model for graph datasets to be reused + printing total run time in secs

--- src/main/scala/mstproject/Main.scala
+++ src/main/scala/mstproject/Main.scala
@@ -7,11 +7,13 @@ import redis.clients.jedis.Jedis
 
 object Main extends App {
     //TODO use Iterator instead of List for memory efficiency.
+    val datasets = List(("src/main/resources/out.moreno_train_train", " ", true), ("src/main/resources/facebook_combined.txt", " ", false), ("src/main/resources/roadNet-CA.txt", "\t" , false), ("src/main/resources/com-orkut.ungraph.txt", "\t", false))
+      .map(d => graphDataset(d._1, d._2, d._3))
     lazy val sampleEdges = List(((1,4),14), ((3,5),5), ((4,6),6), ((1,2),7), ((2,5),3), ((2,3),8), ((5,6),10), ((2,4),11), ((5,7),12), ((6,7),13), ((4,5),15))
-    lazy val morenoEdges = file"src/main/resources/out.moreno_train_train".lines.map( l => l.split(" ").map(_.toInt) ).toList
-    lazy val fbEdges = file"src/main/resources/facebook_combined.txt".lines.map( l => l.split(" ").map(_.toInt) ).toList
-    lazy val caEdges = file"src/main/resources/roadNet-CA.txt".lines.map( l => l.split("\t").map(_.toInt) ).toList
-    lazy val orkutEdges = file"src/main/resources/com-orkut.ungraph.txt".lines.map( l => l.split("\t").map(_.toInt) ).toList
+    lazy val morenoEdges = file"${datasets(0).path}".lines.map( l => l.split(datasets(0).delim).map(_.toInt) ).toList
+    lazy val fbEdges = file"${datasets(1).path}".lines.map( l => l.split(datasets(1).delim).map(_.toInt) ).toList
+    lazy val caEdges = file"${datasets(2).path}".lines.map( l => l.split(datasets(2).delim).map(_.toInt) ).toList
+    lazy val orkutEdges = file"${datasets(3).path}".lines.map( l => l.split(datasets(3).delim).map(_.toInt) ).toList
 
 //    val edges = sampleEdges.zipWithIndex.map(r => weightedEdge( myEdge(r._1._1, r._1._2), r._2, id = r._2))
 //    val edges = morenoEdges.zipWithIndex.map(r => weightedEdge( myEdge(r._1(0), r._1(1)), r._1(2), id = r._2))
@@ -20,6 +22,7 @@ object Main extends App {
 //    val edges = orkutEdges.zipWithIndex.map(r => weightedEdge( myEdge(r._1(0), r._1(1)), r._2, id = r._2 ))
 
 //    println(s"Number of edges= ${edges.length}")
+    val startTime = System.nanoTime
 
 //    val kruskal = Kruskal(edges)
 //    println("Kruskal: " + kruskal.length)//; kruskal.foreach(println)
@@ -29,14 +32,11 @@ object Main extends App {
 //    println("---")
 //    val boruvka = Boruvka(edges)
 //    println("Boruvka: " + boruvka.length)//; boruvka.foreach(println)
-    val boruvkaRDD_Revised = BoruvkaRDD_Revised("src/main/resources/com-orkut.ungraph.txt", "\t")
+    val boruvkaRDD_Revised = BoruvkaRDD_Revised(datasets(2), debug = false)
     println("BoruvkaRDD_Revised: " + boruvkaRDD_Revised.length)//; boruvkaRDD_Revised.foreach(println)
-
-//    val boruvkaRDD_RS = BoruvkaRDD_RS("src/main/resources/facebook_combined.txt", " ")
-//    println("BoruvkaRS: " + boruvkaRDD_RS.length)//; boruvkaDS.foreach(println)
-//    val boruvkaRDD_Revised = BoruvkaRDD_Revised("src/main/resources/out.moreno_train_train", " ", weighted = true)
-//    val boruvkaRDD_Revised = BoruvkaRDD_Revised("src/main/resources/facebook_combined.txt", " ")
-//    println("BoruvkaRDD_Revised: " + boruvkaRDD_Revised.length)//; boruvkaDS.foreach(println)
-//    val boruvkaGraphX = BoruvkaGraphX("src/main/resources/facebook_combined.txt", " ")
+//    val boruvkaGraphX = BoruvkaGraphX(datasets(2))
 //    println("BoruvkaGraphX: " + boruvkaGraphX.length)//; boruvkaGraphX.foreach(println)
+
+    val duration = (System.nanoTime - startTime) / 1e9d // show in seconds
+    println("Duration: " + duration)
 }
