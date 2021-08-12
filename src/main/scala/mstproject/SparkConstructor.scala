58b4754ff69a9a23415d08638e759a3907c1eec7
MalekMFS <malekabbasi@meam.ir>
2021-08-12 12:24:39 +0430

    Comment redundant spark configs

--- src/main/scala/mstproject/SparkConstructor.scala
+++ src/main/scala/mstproject/SparkConstructor.scala
@@ -3,15 +3,15 @@ package mstproject
 import org.apache.spark.sql.SparkSession
 
 object SparkConstructor {
-  def apply() = {
+  def apply(): SparkSession = {
     val ss = SparkSession.builder
       .appName("mst-project")
       .master("local[*]")
       //    .master("spark://127.0.0.1:7077") //for production
-      .config("spark.driver.memory", "8g")
-      .config("spark.driver.memoryOverhead", "1g")
-      .config("spark.executor.memory", "8g")
-      .config("spark.executor.memoryOverhead", "1g")
+//      .config("spark.driver.memory", "8g")
+//      .config("spark.driver.memoryOverhead", "1g")
+//      .config("spark.executor.memory", "8g")
+//      .config("spark.executor.memoryOverhead", "1g")
       .config("spark.redis.host", "localhost") // for redisSpark
       .config("spark.redis.port", "6379")
       .getOrCreate()
